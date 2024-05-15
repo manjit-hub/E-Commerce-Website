56c8f3566369cb2e64c50066c12e09b72228860f
Manjit Majhi <manjitmajhi156@gmail.com>
2024-05-15 23:20:35 +0530

    FinalAddition

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ function menutoggle(){
         MenuItems.style.maxHeight = "0px";
     }
 }
+
+// -----
+
+var ProductImg = document.getElementById("product-img");
+    var SmallImg = document.querySelectorAll(".small-img-col img");
+
+    SmallImg[0].onclick = function() {
+        ProductImg.src = this.src; // Use "this" to refer to the clicked image
+    }
+    SmallImg[1].onclick = function() {
+        ProductImg.src = this.src;
+    }
+    SmallImg[2].onclick = function() {
+        ProductImg.src = this.src;
+    }
+    SmallImg[3].onclick = function() {
+        ProductImg.src = this.src;
+    }
+
+    // -----
+
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var indicator = document.getElementById("indicator");
+
+function register(){
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    indicator.style.transform = "translateX(100px)";
+}
+
+function login(){
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    indicator.style.transform = "translateX(0px)";
+}
+
